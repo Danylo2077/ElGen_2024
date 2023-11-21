f2da1c9b633d9a31a19b37dd054abf7b2bc5ff22
Vladyslav <132401201+Amnezii@users.noreply.github.com>
2023-11-21 20:39:27 +0100

    upload repository

--- back-end/src/main/java/com/elgen/repository/ChatRepository.java
+++ back-end/src/main/java/com/elgen/repository/ChatRepository.java
@@ -1,9 +1,9 @@
 package com.elgen.repository;
 
-import com.elgen.model.Message;
+import com.elgen.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface ChatRepository extends JpaRepository<Chat, Long> {
 }
